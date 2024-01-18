7b832421b25825655a6f1212645e0eebe5c51d50
Douglas de Melo <dougmelo2017@gmail.com>
2024-01-17 22:03:19 -0300

    aula-10-Recursos nativo do navegador<

---
+++ JavaScript-modulo-01/aula-10/index.js
@@ -0,0 +1,18 @@
+let name = "Douglas"
+
+console.log(name)
+console.error(name)
+console.warn(name)
+
+//window.alert(name)
+//name = window.prompt("Insira seu nome")
+//let confirmacao = window.confirm("Confirme essa ação")
+
+alert("olá, mundo")
+prompt("olá")
+confirm("confirme")
+
+// window é o objeto global do navegador podendo usar suas funções diretamente.
+
+
+
