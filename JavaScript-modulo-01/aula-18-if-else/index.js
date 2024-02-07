a7b7ae84fb9dd4df571175b6716491267d51c53d
Douglas de Melo <dougmelo2017@gmail.com>
2024-02-06 22:47:35 -0300

    aula 18 - IF  e  ELSE

---
+++ JavaScript-modulo-01/aula-18-if-else/index.js
@@ -0,0 +1,42 @@
+
+const idade = prompt("Informe sua idade:")
+
+/*
+desta forma o codigo fica dificil de leitura!!
+
+if (idade > 18){
+    alert("Você é maior de idade")
+}else{
+    alert("Você é menor de idade")
+    if (idade < 12){
+        alert("Você é criança")
+        if(idade < 4){
+         alert("")
+        }
+    }
+}
+*/
+
+/*
+Desta forma o codigo fica mais simples para leitura.
+ */
+if (idade > 18){
+    alert("Você é maior de idade")
+} else if (idade > 12){
+    alert("Você é menor de idade")
+} else if(idade > 4){
+    alert("Você é criança")
+} else{
+    alert("..")
+}
+
+/* 
+    sintaxe mais simples:
+    Forma abreviada e autoavaliada do IF ELSE
+    ? === IF
+    : === ELSE
+ */
+
+    const resultado = (6 === 6) ? "Vedadeiro" : "Falso"
+    console.log(resultado)
+
